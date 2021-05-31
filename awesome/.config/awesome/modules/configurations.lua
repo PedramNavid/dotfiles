a6420f0b98719cb3d746900c190409b434100ef1
Pedram Navid <pedram@hey.com>
2021-05-30 20:03:10 -0700

    clean up layouts in awesome

--- awesome/.config/awesome/modules/configurations.lua
+++ awesome/.config/awesome/modules/configurations.lua
@@ -3,16 +3,24 @@ local layouts = require('modules.layouts')
 
 local chosen_theme = 'pedram'
 
-awful.util.tagnames = {"", "","", "", "", "", "" }
+awful.util.tagnames = {
+    "", -- 1
+    "", -- 2
+    "", -- 3
+    "", -- 4
+    "", -- 5
+    "", -- 6
+    ""  -- 7
+}
 
 local mylayouts = {
-    layouts[1],
-    layouts[2],
-    layouts[2],
-    layouts[5],
-    layouts[5],
-    layouts[5],
-    layouts[6]
+    layouts[2],  -- 1
+    layouts[2],  -- 2
+    layouts[1],  -- 3
+    layouts[1],  -- 4
+    layouts[8],  -- 5
+    layouts[7],  -- 6
+    layouts[8]   -- 7
 }
 
 return {
