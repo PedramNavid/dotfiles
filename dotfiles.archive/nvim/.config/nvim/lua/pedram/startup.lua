ec7a282d2ecbc663f216be7550c64cc564a4a1cf
Pedram Navid <pedram.navid@gmail.com>
2022-10-09 14:11:18 -0700

    big old chang

--- dotfiles.archive/nvim/.config/nvim/lua/pedram/startup.lua
+++
@@ -1,5 +0,0 @@
-require('pedram.devicons')
-require('pedram.globals')
-require('pedram.lsp-bindings')
-require('pedram.telescope')
-require('pedram.treesitter')
--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -57,4 +57,3 @@ vim.o.hidden = true
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.updatetime = 300
-
