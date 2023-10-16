dc3052c670a8e44bea5137dff0d11b54eb275066
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2023-10-15 20:09:26 -0700

    fix tree

--- nvim/lua/pedram/plugins/nvim-tree.lua
+++ nvim/lua/pedram/plugins/nvim-tree.lua
@@ -1,12 +1,19 @@
 return {
     "nvim-tree/nvim-tree.lua",
     config = function()
+        local function toggle()
+            local api = require("nvim-tree.api")
+            if api.tree.is_visible() then
+                api.tree.close()
+            else
+                vim.cmd("NvimTreeFocus")
+            end
+        end
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
         require('nvim-tree').setup()
 
-        vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<C-n>', toggle, { noremap = true, silent = true })
     end
 
 }
