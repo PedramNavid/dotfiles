e23dc5c26729d6fd687def01565dff32b6b96493
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2023-12-24 17:21:59 -0800

    add mappings

--- nvim/lua/pedram/mappings.lua
+++ nvim/lua/pedram/mappings.lua
@@ -4,3 +4,4 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 map("n", "<Leader>xx", ":Explore<cr>", { desc = "Open File Explorer" })
+map("n", "<Leader>s", ":write<CR>", { desc = "Save file" })
