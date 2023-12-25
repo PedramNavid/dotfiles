092a6ed2cb46ef502ebc4fdd2549cd9b82ad55b4
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2023-12-24 18:21:06 -0800

    update trouble/mapping

--- nvim/lua/pedram/mappings.lua
+++ nvim/lua/pedram/mappings.lua
@@ -3,5 +3,5 @@ local map = vim.keymap.set
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
-map('n', '<Leader>xx', ':Explore<cr>', { desc = 'Open File Explorer' })
+map('n', '<Leader>ex', ':Explore<cr>', { desc = 'Open File Explorer' })
 map('n', '<Leader>s', ':write<CR>', { desc = 'Save file' })
--- nvim/lua/pedram/plugins/trouble.lua
+++ nvim/lua/pedram/plugins/trouble.lua
@@ -2,20 +2,18 @@ return {
 	'folke/trouble.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
-		position = 'bottom', -- position of the list can be: bottom, top, left, right
-		height = 10, -- height of the trouble list when position is top or bottom
-		width = 50, -- width of the list when position is left or right
-		icons = true, -- use devicons for filenames
-		mode = 'workspace_diagnostics', -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-		severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
-		fold_open = '', -- icon used for open folds
-		fold_closed = '', -- icon used for closed folds
-		group = true, -- group results by file
-		padding = true, -- add an extra new line on top of the list
-		cycle_results = true, -- cycle item list when reaching beginning or end of list
-		action_keys = { -- key mappings for actions in the trouble list
-			-- map to {} to remove a mapping, for example:
-			-- close = {},
+		position = 'bottom',
+		height = 10,
+		width = 50,
+		icons = true,
+		mode = 'workspace_diagnostics',
+		severity = nil,
+		fold_open = '',
+		fold_closed = '',
+		group = true,
+		padding = true,
+		cycle_results = true,
+		action_keys = {
 			close = 'q', -- close the list
 			cancel = '<esc>', -- cancel the preview and get back to your last window / buffer / cursor
 			refresh = 'r', -- manually refresh
@@ -37,23 +35,30 @@ return {
 			next = 'j', -- next item
 			help = '?', -- help menu
 		},
-		multiline = true, -- render multi-line messages
-		indent_lines = true, -- add an indent guide below the fold icons
-		win_config = { border = 'single' }, -- window configuration for floating windows. See |nvim_open_win()|.
-		auto_open = false, -- automatically open the list when you have diagnostics
-		auto_close = false, -- automatically close the list when you have no diagnostics
-		auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-		auto_fold = false, -- automatically fold a file trouble list at creation
-		auto_jump = { 'lsp_definitions' }, -- for the given modes, automatically jump if there is only a single result
-		include_declaration = { 'lsp_references', 'lsp_implementations', 'lsp_definitions' }, -- for the given modes, include the declaration of the current symbol in the results
+		multiline = true,
+		indent_lines = true,
+		win_config = { border = 'single' },
+		auto_open = false,
+		auto_close = false,
+		auto_preview = true,
+		auto_fold = false,
+		auto_jump = { 'lsp_definitions' },
+		include_declaration = { 'lsp_references', 'lsp_implementations', 'lsp_definitions' },
 		signs = {
-			-- icons / text used for a diagnostic
 			error = '',
 			warning = '',
 			hint = '',
 			information = '',
 			other = '',
 		},
-		use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+		use_diagnostic_signs = false,
+	},
+	keys = {
+		{ '<leader>xx', '<cmd>TroubleToggle<cr>', desc = 'Toggle Trouble' },
+		{ '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>' },
+		{ '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>' },
+		{ '<leader>xq', '<cmd>TroubleToggle quickfix<cr>' },
+		{ '<leader>xl', '<cmd>TroubleToggle loclist<cr>' },
+		{ 'gR', '<cmd>TroubleToggle lsp_references<cr>' },
 	},
 }
