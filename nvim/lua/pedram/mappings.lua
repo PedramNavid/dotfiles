67e223c33a3a311cbc19c9808a11e5caae96c45a
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2023-12-24 17:24:49 -0800

    add mappings/fix stylua

--- nvim/lua/pedram/mappings.lua
+++ nvim/lua/pedram/mappings.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
 
-map("n", "<Leader>xx", ":Explore<cr>", { desc = "Open File Explorer" })
-map("n", "<Leader>s", ":write<CR>", { desc = "Save file" })
+map('n', '<Leader>xx', ':Explore<cr>', { desc = 'Open File Explorer' })
+map('n', '<Leader>s', ':write<CR>', { desc = 'Save file' })
