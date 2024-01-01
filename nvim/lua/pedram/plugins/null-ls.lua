a98e717b588b9ac79233a3e955f5deb239715f54
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-01-01 11:38:39 -0800

    .

--- nvim/lua/pedram/plugins/null-ls.lua
+++ nvim/lua/pedram/plugins/null-ls.lua
@@ -5,9 +5,19 @@ return {
 	config = function()
 		local null_ls = require('null-ls')
 		null_ls.setup({
-			-- set root directory to cwd
-			root_dir = function() return nil end,
-			sources = { null_ls.builtins.formatting.stylua },
+			debug = true,
+			sources = {
+				null_ls.builtins.diagnostics.ruff.with({
+					command = '/home/pedram/.pyenv/shims/ruff',
+				}),
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.ruff.with({
+					command = '/home/pedram/.pyenv/shims/ruff',
+				}),
+				null_ls.builtins.formatting.ruff_format.with({
+					command = '/home/pedram/.pyenv/shims/ruff',
+				}),
+			},
 		})
 	end,
 	keys = { { '<LocalLeader>f', vim.lsp.buf.format } },
