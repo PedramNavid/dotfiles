353337e6daa052d7521e5ad93673e66d2218f9ce
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-03-03 23:34:29 -0800

    .

--- awesome.bak/config/apps.lua
+++
@@ -1,8 +0,0 @@
-local apps = {}
-local defaults = require('config.defaults')
-
-apps.terminal = defaults.terminal
-apps.editor_cmd = defaults.editor_cmd
-apps.browser = defaults.browser
-
-return apps
