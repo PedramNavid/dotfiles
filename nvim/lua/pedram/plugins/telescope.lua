ffd665fba37436a152e72d14279cff672457b888
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2025-06-22 15:04:49 -0700

    feat(nvim): enhance configuration with git keymaps and treesitter textobjects
    Add new language-specific rules, optimize plugin loading, and improve completion.
    Integrate treesitter textobjects for better code navigation and manipulation.
    Update model to claude-3-7-sonnet-latest.

--- nvim/lua/pedram/plugins/telescope.lua
+++ nvim/lua/pedram/plugins/telescope.lua
@@ -13,11 +13,29 @@ return {
   config = function()
     local telescope = require 'telescope'
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope Files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope Grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope Buffers' })
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope keymaps' })
-    vim.keymap.set('n', '<leader>fh', builtin.oldfiles, { desc = 'Telescope history' })
+    -- File and text search
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Grep Current Word' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find Buffers' })
+    vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Find Old Files' })
+
+    -- LSP and diagnostics
+    vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Find References' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Find Diagnostics' })
+    vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Find Symbols' })
+    vim.keymap.set('n', '<leader>fS', builtin.lsp_workspace_symbols, { desc = 'Find Workspace Symbols' })
+
+    -- Git
+    vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Git Commits' })
+    vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Git Branches' })
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Git Status' })
+
+    -- Other useful pickers
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Find Keymaps' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find Help' })
+    vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = 'Find Commands' })
+    vim.keymap.set('n', '<leader>ft', builtin.builtin, { desc = 'Find Telescope Builtins' })
 
     telescope.setup {
       defaults = {
@@ -38,7 +56,26 @@ return {
             preview_cutoff = 40,
           },
         },
-        mappings = {},
+        mappings = {
+          i = {
+            ['<C-u>'] = false,
+            ['<C-d>'] = false,
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          find_command = { 'rg', '--files', '--hidden', '--glob', '!.git/*' },
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+        },
       },
     }
     require('telescope').load_extension 'fzf'
